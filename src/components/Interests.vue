c9e254f6d17ac3ac06418498e1745757bbb50e32
vladimirdashuk <donvov1990@gmail.com>
2022-07-25 18:40:48 +0300

    🧭 Adjusts about me content.

--- src/components/Interests.vue
+++ src/components/Interests.vue
@@ -13,16 +13,37 @@
             <span class="comment">*</span>
         </div>
         <div data-line-nr="04" class="code-line">
-            <span class="comment">*</span> kayaking
+            <span class="comment">*</span> I have different interests and
         </div>
         <div data-line-nr="05" class="code-line">
-            <span class="comment">*</span> biking
+            <span class="comment">*</span> one of them is kayaking &#128675;,
         </div>
         <div data-line-nr="06" class="code-line">
-            <span class="comment">*</span> playing video games
+            <span class="comment">*</span>  which came to me quite recently.
         </div>
         <div data-line-nr="07" class="code-line">
-            <span class="comment">*</span> rock
+            <span class="comment">*</span> I got to know a group of people
+        </div>
+        <div data-line-nr="08" class="code-line">
+            <span class="comment">*</span> who do weekend kayaking
+        </div>
+        <div data-line-nr="09" class="code-line">
+            <span class="comment">*</span> trips together in picturesque places.
+        </div>
+        <div data-line-nr="10" class="code-line">
+            <span class="comment">*</span> I also love to ride my
+        </div>
+        <div data-line-nr="11" class="code-line">
+            <span class="comment">*</span> bike, play video games,
+        </div>
+        <div data-line-nr="12" class="code-line">
+            <span class="comment">*</span> listen the rock
+        </div>
+        <div data-line-nr="13" class="code-line">
+            <span class="comment">*</span> and enjoy life!
+        </div>
+        <div data-line-nr="14" class="code-line">
+            <span class="comment">*&#x2f;</span>
         </div>
     </div>
 </template>
